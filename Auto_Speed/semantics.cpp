47f297441f5e8ab53e10c6a89c009d35a4b966eb
ASMmahran <138701683+ASMmahran@users.noreply.github.com>
2025-12-10 11:09:26 +0200

    new

--- Auto_Speed/semantics.cpp
+++ Auto_Speed/semantics.cpp
@@ -4,6 +4,8 @@
 using namespace std;
 
 SemanticAnalyzer::SemanticAnalyzer() : hasErr(false) {
+    // create global (outermost) scope
+    scopes.emplace_back();
 }
 
 bool SemanticAnalyzer::hasErrors() const {
@@ -15,20 +17,44 @@ void SemanticAnalyzer::reportError(const string& msg, const Token& t) {
     cerr << "Semantic Error [Line " << t.line << "]: " << msg << " (near '" << t.value << "')" << endl;
 }
 
+// ----- scope management -----
+void SemanticAnalyzer::enterScope() {
+    scopes.emplace_back();
+}
+
+void SemanticAnalyzer::leaveScope() {
+    if (!scopes.empty()) scopes.pop_back();
+    // keep at least the global scope if you prefer:
+    // if (scopes.empty()) scopes.emplace_back();
+}
+
+// ----- variable handling -----
 void SemanticAnalyzer::declareVariable(const string& name, const string& type, const Token& t) {
-    if (vars.count(name)) {
-        reportError("Variable '" + name + "' already declared", t);
+    if (scopes.empty()) scopes.emplace_back();
+
+    auto& curr = scopes.back();
+    if (curr.count(name)) {
+        reportError("Variable '" + name + "' already declared in this scope", t);
         return;
     }
-    vars[name] = type;
+    curr[name] = type;
 }
 
 void SemanticAnalyzer::assignVariable(const string& name, const Token& t) {
-    if (!vars.count(name)) {
-        reportError("Variable '" + name + "' used before declaration", t);
+    // lookup from top down
+    for (auto it = scopes.rbegin(); it != scopes.rend(); ++it) {
+        if (it->count(name)) return; // found
     }
+    reportError("Variable '" + name + "' used before declaration (or out of scope)", t);
 }
 
+bool SemanticAnalyzer::isDeclaredInCurrentScope(const string& name) const {
+    if (scopes.empty()) return false;
+    const auto& curr = scopes.back();
+    return curr.count(name) > 0;
+}
+
+// ----- functions -----
 void SemanticAnalyzer::declareFunction(const string& name, int paramCount, const Token& t) {
     if (funcs.count(name)) {
         reportError("Function '" + name + "' already declared", t);
